7af497d83e5d3a968237eb02bb03dc8270cb18cd
DavidJimenezS <56040057+DavidJimenezS@users.noreply.github.com>
2019-10-08 10:51:43 -0500

    Update binary_kappa.m

--- MMT-G/binary_kappa.m
+++ MMT-G/binary_kappa.m
@@ -24,35 +24,12 @@ Iaux3_nofilt = ((reshape(Iaux3_nofilt,m_c,n_c)));
 
 clear U Eval m_c n_c locations complement A AB
 
-%% 95 96 results
-% Iaux3_filt = medfilt2(Iaux3,[3 1]);
-% 
-% Ibw = imbinarize(Iaux3_filt);
-% % imshow(Ibw,[])
-% 
-% IbwOp = bwareaopen(Ibw,1000);
-% % imshow(IbwOp,[])
-% 
-% SE = strel('cube',2);
-% IbwEr = imerode(IbwOp,SE);
-% %imshow(IbwEr,[])
-% 
-% Ibw1 = bwareaopen(IbwEr,700);
-% figure,imshow(Ibw1,[])
-% title('Change map using MMG with filter','FontSize',13,'interpreter','latex')
-% set(gca,'FontSize',12)
-
 
 Ibw1_lake = imbinarize(Iaux3_nofilt);
 figure, imshow(Ibw1_lake,[])
 title('Change map using MMG','FontSize',13,'interpreter','latex')
 set(gca,'FontSize',12)
 
-% Ibw3 = imbinarize(Iaux3_silverman);
-% figure, imshow(Ibw3,[])
-% title('Change map using MMG Silv','FontSize',13,'interpreter','latex')
-% set(gca,'FontSize',12)
-% 
 
 rR_bw_lake = imbinarize(change_map);
 figure, imshow(rR_bw_lake,[])
@@ -80,7 +57,6 @@ set(gca,'FontSize',12)
 
 MA = zeros(4,1); FA = MA; Precision = MA; recall = MA; kappa = MA; OE = MA;
 methods = {'rR-EM';'rrR-EM';'KI';'MMG'};
-%methods = {'rR-EM';'rrR-EM'};
 
 disp('Results for lake Mulargia flood event')
 
@@ -94,92 +70,10 @@ disp('Results for lake Mulargia flood event')
 
 T1 = table(MA,FA,Precision,recall,kappa,OE,'RowNames',methods)
 
+
 %% ---------------------------------- ----------------
 
 %-----------------Data set 2---------------------
-
-
-%% Correcting locations of the samples pixels
-%-----------------Data-------------------------
-
-% load('contest_cub.mat'), load('gt_contest.mat'),  load('rR_contest.mat'), load('rrR_contest.mat')
-% 
-% 
-% Iaux3_nofilt = (U(:,1))*sqrt(Eval);
-% 
-% n = length(locations);
-% A = Iaux3_nofilt(1:n);
-% AB = Iaux3_nofilt(n+1:end);
-% Iaux3_nofilt(locations) = A;
-% Iaux3_nofilt(complement) = AB;
-% Iaux3_nofilt = ((reshape(Iaux3_nofilt,m_c,n_c)));
-% 
-% clear U Eval m_c n_c locations complement A AB
-% 
-% %% Contest results
-% 
-% % Iaux3_filt = medfilt2(Iaux3,[2 1]);
-% % 
-% % Ibw = imbinarize(Iaux3_filt);
-% % imshow(Ibw,[])
-% % 
-% % IbwOp = bwareaopen(Ibw,20000);
-% % imshow(IbwOp,[])
-% % 
-% % SE = strel('cube',5);
-% % IbwEr = imerode(IbwOp,SE);
-% % imshow(IbwEr,[])
-% % 
-% % Ibw1 = bwareaopen(IbwEr,20000);
-% % figure, imshow(Ibw1,[])
-% % title('Change map using MMG','FontSize',13,'interpreter','latex')
-% % set(gca,'FontSize',12)
-% 
-% 
-% Ibw1 = imbinarize(Iaux3_nofilt);
-% figure, imshow(Ibw2,[])
-% title('Change map using MMG 3','FontSize',13,'interpreter','latex')
-% set(gca,'FontSize',12)
-% 
-% rR_bw = imbinarize(change_map);
-% figure, imshow(rR_bw,[])
-% title('Change map using rR-EM','FontSize',13,'interpreter','latex')
-% set(gca,'FontSize',12)
-% 
-% rrR_bw = imbinarize(rrR_contest_map);
-% figure, imshow(rrR_bw,[])
-% title('Change map using rrR-EM','FontSize',13,'interpreter','latex')
-% set(gca,'FontSize',12)
-% 
-% gtc_bw = imbinarize(gt);
-% figure, imshow(gtc_bw,[])
-% title('Reference Change map','FontSize',13,'interpreter','latex')
-% set(gca,'FontSize',12)
-% 
-% 
-% MA = zeros(5,1); FA = MA; Precision = MA; recall = MA; kappa = MA; OE = MA;
-% %methods = {'MMG';'rR-EM';'inverse rR-EM'};
-% methods = {'rR-EM';'rrR-EM';'MMG filt';'MMG 3';'MMG Silv'};
-% 
-% 
-% %I3_bw = imbinarize(Iaux3);
-% 
-% [MA(1), FA(1), Precision(1), recall(1), kappa(1), OE(1)] = cohensKappa(gtc_bw(:),rR_bw(:));
-% 
-% [MA(2), FA(2), Precision(2), recall(2), kappa(2), OE(2)] = cohensKappa(gtc_bw(:),rrR_bw(:));
-% 
-% [MA(3), FA(3), Precision(3), recall(3), kappa(3), OE(3)] = cohensKappa(gtc_bw(:),Ibw1(:));
-% 
-% [MA(4), FA(4), Precision(4), recall(4), kappa(4), OE(4)] = cohensKappa(gtc_bw(:),Ibw2(:));
-% 
-% [MA(5), FA(5), Precision(5), recall(5), kappa(5), OE(5)] = cohensKappa(gtc_bw(:),Ibw3(:));
-% disp('Results for DF contest flood event')
-% 
-% table(MA,FA,Precision,recall,kappa,OE,'RowNames',methods)
-
-%% ---------------------------------- ----------------
-
-%-----------------Data set 3---------------------
 
 
 %% Correcting locations of the samples pixels
@@ -262,25 +156,6 @@ E_lake_rR(aux_1 == 2)= 1;
 rR_label_lake = label2rgb(E_lake_rR,[0 1 0;0 0 1; 1 0 0],[1 1 1]);
 h = figure; imshow(rR_label_lake)
 title('Error map using rR-EM','FontSize',13,'interpreter','latex')
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'Correct';
-% t.Color = 'green';
-% pos = get(gca, 'Position');
-% t.Position = [pos(1)+0.05 pos(2)+0.5 0.1 0.04];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'MA';
-% t.Color = 'blue';
-% t.Position = [pos(1)+0.05 pos(2)+0.46 0.1 0.04];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'FA';
-% t.Color = 'red';
-% t.Position = [pos(1)+0.05 pos(2)+0.42 0.1 0.04];
 set(gca,'FontSize',12)
 export_fig(strcat(pwd,'\Figures\rR-EM_Emap_lake'),'-pdf','-transparent',h)
 
@@ -294,25 +169,6 @@ E_lake_rrR(aux_1 == 2)= 1;
 rrR_label_lake = label2rgb(E_lake_rrR,[0 1 0;0 0 1; 1 0 0],[1 1 1]);
 h = figure; imshow(rrR_label_lake)
 title('Error map using rrR-EM','FontSize',13,'interpreter','latex')
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'Correct';
-% t.Color = 'green';
-% pos = get(gca, 'Position');
-% t.Position = [pos(1)+0.05 pos(2)+0.5 0.1 0.04];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'MA';
-% t.Color = 'blue';
-% t.Position = [pos(1)+0.05 pos(2)+0.46 0.1 0.04];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'FA';
-% t.Color = 'red';
-% t.Position = [pos(1)+0.05 pos(2)+0.42 0.1 0.04];
 set(gca,'FontSize',12)
 export_fig(strcat(pwd,'\Figures\rrR-EM_Emap_lake'),'-pdf','-transparent',h)
 
@@ -327,25 +183,6 @@ E_lake_KI(aux_1 == 2)= 1;
 KI_label_lake = label2rgb(E_lake_KI,[0 1 0;0 0 1; 1 0 0],[1 1 1]);
 h = figure; imshow(KI_label_lake)
 title('Error map using KI','FontSize',13,'interpreter','latex')
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'Correct';
-% t.Color = 'green';
-% pos = get(gca, 'Position');
-% t.Position = [pos(1)+0.05 pos(2)+0.5 0.1 0.04];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'MA';
-% t.Color = 'blue';
-% t.Position = [pos(1)+0.05 pos(2)+0.46 0.1 0.04];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'FA';
-% t.Color = 'red';
-% t.Position = [pos(1)+0.05 pos(2)+0.42 0.1 0.04];
 set(gca,'FontSize',12)
 export_fig(strcat(pwd,'\Figures\KI_Emap_lake'),'-pdf','-transparent',h)
 
@@ -360,25 +197,6 @@ E_lake_MMG(aux_1 == 2)= 1;
 MMG_label_lake = label2rgb(E_lake_MMG,[0 1 0;0 0 1; 1 0 0],[1 1 1]);
 h = figure; imshow(MMG_label_lake)
 title('Error map using MMG','FontSize',13,'interpreter','latex')
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'Correct';
-% t.Color = 'green';
-% pos = get(gca, 'Position');
-% t.Position = [pos(1)+0.05 pos(2)+0.5 0.1 0.04];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'MA';
-% t.Color = 'blue';
-% t.Position = [pos(1)+0.05 pos(2)+0.46 0.1 0.04];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'FA';
-% t.Color = 'red';
-% t.Position = [pos(1)+0.05 pos(2)+0.42 0.1 0.04];
 set(gca,'FontSize',12)
 export_fig(strcat(pwd,'\Figures\MMG_Emap_lake'),'-pdf','-transparent',h)
 
@@ -393,25 +211,6 @@ E_fire_rR(aux_1 == 2)= 1;
 rR_label_fire = label2rgb(E_fire_rR,[0 1 0;0 0 1; 1 0 0],[1 1 1]);
 h = figure; imshow(rR_label_fire)
 title('Error map using rR-EM','FontSize',13,'interpreter','latex')
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'Correct';
-% t.Color = 'green';
-% pos = get(gca, 'Position');
-% t.Position = [pos(1)+0.05 pos(2)+0.5 0.1 0.03];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'MA';
-% t.Color = 'blue';
-% t.Position = [pos(1)+0.05 pos(2)+0.47 0.1 0.03];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'FA';
-% t.Color = 'red';
-% t.Position = [pos(1)+0.05 pos(2)+0.44 0.1 0.03];
 set(gca,'FontSize',12)
 export_fig(strcat(pwd,'\Figures\rR-EM_Emap'),'-pdf','-transparent',h)
 
@@ -426,26 +225,6 @@ E_fire_rrR(aux_1 == 2)= 1;
 rrR_label_fire = label2rgb(E_fire_rrR,[0 1 0;0 0 1; 1 0 0],[1 1 1]);
 h = figure; imshow(rrR_label_fire)
 title('Error map using rrR-EM','FontSize',13,'interpreter','latex')
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'Correct';
-% t.Color = 'green';
-% pos = get(gca, 'Position');
-% t.Position = [pos(1)+0.05 pos(2)+0.5 0.1 0.03];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'MA';
-% t.Color = 'blue';
-% t.Position = [pos(1)+0.05 pos(2)+0.47 0.1 0.03];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'FA';
-% t.Color = 'red';
-% t.Position = [pos(1)+0.05 pos(2)+0.44 0.1 0.03];
-% set(gca,'FontSize',12)
 set(gca,'FontSize',12)
 export_fig(strcat(pwd,'\Figures\rrR-EM_Emap'),'-pdf','-transparent',h)
 
@@ -460,26 +239,6 @@ E_fire_KI(aux_1 == 2)= 1;
 KI_label_fire = label2rgb(E_fire_KI,[0 1 0;0 0 1; 1 0 0],[1 1 1]);
 h = figure; imshow(KI_label_fire)
 title('Error map using KI','FontSize',13,'interpreter','latex')
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'Correct';
-% t.Color = 'green';
-% pos = get(gca, 'Position');
-% t.Position = [pos(1)+0.05 pos(2)+0.5 0.1 0.03];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'MA';
-% t.Color = 'blue';
-% t.Position = [pos(1)+0.05 pos(2)+0.47 0.1 0.03];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'FA';
-% t.Color = 'red';
-% t.Position = [pos(1)+0.05 pos(2)+0.44 0.1 0.03];
-% set(gca,'FontSize',12)
 set(gca,'FontSize',12)
 export_fig(strcat(pwd,'\Figures\KI_Emap'),'-pdf','-transparent',h)
 
@@ -494,24 +253,5 @@ E_fire_MMG(aux_1 == 2)= 1;
 MMG_label_fire = label2rgb(E_fire_MMG,[0 1 0;0 0 1; 1 0 0],[1 1 1]);
 h = figure; imshow(MMG_label_fire)
 title('Error map using MMG','FontSize',13,'interpreter','latex')
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'Correct';
-% t.Color = 'green';
-% pos = get(gca, 'Position');
-% t.Position = [pos(1)+0.05 pos(2)+0.5 0.1 0.03];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'MA';
-% t.Color = 'blue';
-% t.Position = [pos(1)+0.05 pos(2)+0.47 0.1 0.03];
-% t = annotation('textbox');
-% t.FontWeight = 'bold';
-% t.BackgroundColor = [0.9490 0.9490 0.9490];
-% t.String = 'FA';
-% t.Color = 'red';
-% t.Position = [pos(1)+0.05 pos(2)+0.44 0.1 0.03];
 set(gca,'FontSize',12)
 export_fig(strcat(pwd,'\Figures\MMG_Emap'),'-pdf','-transparent',h)
